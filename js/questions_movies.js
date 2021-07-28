5f82529a16d34961260baddb5d717cfc71528eb8
thxare <thiareayleen@gmail.com>
2021-07-27 23:49:11 -0400

    se han anadido los cambios

--- js/questions_movies.js
+++ js/questions_movies.js
@@ -2,53 +2,73 @@
 $(function () {
     let div_movies = $('#questions_mov');
     let getUserName = localStorage.getItem('name'); $('#movies_title').append("Bienvenid@ " + getUserName + "! Elegiste la categoría Movies :) ");
-    //console.log(getUserName)
     bringMovies()
         .then(res => res.json())
         .then(data => {
             listadoPreguntasMovies(data);
-            //listadoRespuestasMovies(data)
-            console.log(data)
+            listadoRespuestas(data);
+
+            //console.log(data)
+        })
+        .catch(function (e) {
+            console.log("Hubo un problema: " + e.message);
         });
+
+
     function bringMovies() { return fetch('https://trivia.willfry.co.uk/api/questions?categories=movies&limit=20') };
 
     function listadoPreguntasMovies(data) {
         data.map((questions, i) => {
             i++;
-            //let pregunta = document.createElement('p');    
-            //let pregunta = i + ". " + questions.question; 
-            const preguntas = [questions.question];
+            let preguntas = [questions.question];
+
+            preguntas = i + ". " + preguntas;
+
+            let questions_m = $('<p>')
+                .attr("id", "pregunta" + i)
+                .attr('class', 'p_question')
+                .attr('style', 'display: none')
+                .text(preguntas);
+
+            $('#questions_mov').append(questions_m);
+
+
+            $('.spinner').css('display', 'none');
+            $('.next').show();
+
+        })
+
+        let show = $('.p_question')[0];
+        show.style = 'display: inline';
+        var a = 0;
+        var sumar_q = function () {
+            $('#next_movie').on('click', function () {            
+                show.style= 'display: none';
+                a++;
+                show = $('.p_question')[a];
+                show.style = 'display: inline';
+            });
+        }; $(sumar_q);
+
+        console.log(show);
+    };
+
+    function listadoRespuestas(data) {
+        data.map((questions) => {
             let respuestas_i = [questions.incorrectAnswers][0].slice(0, 3);
             let respuesta_c = [questions.correctAnswer][0];
-            
-            let pregunta_i = i + ". " + preguntas;
-
             let respuestas_ordenadas = respuestas_i.push(respuesta_c);
 
             respuestas_i.sort(() => {
                 return Math.random() - 0.5;
             });
 
-            console.log(pregunta_i);
+            respuestas_i.forEach(element => {
+
+                console.log(element);
+            });
+
             console.log(respuestas_i);
-            //let sl =respuestas_i[0].slice(0,3);
-            //console.log(preguntas);
-            //console.log(respuestas_i);
-            //console.log(respuesta_c)
-            //$('#question_list').append('<li>' + pregunta_i +'<br>' +respuestas_i+ '</li>').css("list-style", "none");
-            //div_movies.append(pregunta); 
-            $('.spinner').css('display', 'none');
-            $('.next').show();
         });
-    };
-
-    // function listadoRespuestasMovies(data){
-    //     data.map((answers,i) =>{
-    //         i++;
-    //         let respuesta = document.createElement('p');
-    //         respuesta.innerHTML= i + ". " + answers.incorrectAnswers;
-    //         div_movies.append(respuesta);
-    //     })
-    // }
-
+    }
 });
