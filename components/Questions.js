25cb60fed9f8d8f667b76bd7f6cd496332bac6bb
Thiare <thiareayleen@gmail.com>
2023-03-05 14:14:23 -0300

    se agrega tailwind

---
+++ components/Questions.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Questions = () => {
+  return (
+    <div className=''>Questions</div>
+  )
+}
--- src/triviaApp.js
+++ src/triviaApp.js
@@ -8,13 +8,10 @@ export const TriviaApp = () => {
   ]);
   return (
     <div>
-      <h1>Welcome to Trivia Game!</h1>
+      <h1 className="font-sans">Welcome to Trivia Game!</h1>
       <form id="form" autocomplete="off">
         <label for="fname">Choose a name:</label>
         <input type="text" id="input_name" />
-        <button type="button" id="btn_start" class="button">
-          Start
-        </button>
       </form>
 
       <label id="category_label">Choose a category:</label>
@@ -23,6 +20,9 @@ export const TriviaApp = () => {
           return <option value={op}>{op}</option>;
         })}
       </select>
+      <button type="button" id="btn_start">
+        Start
+      </button>
     </div>
   );
 };
